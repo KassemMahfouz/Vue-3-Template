c3f61165c846be09cc64decfe911e41e9a114b50
Kassem Mahfouz <kassemmhz@gmail.com>
2023-01-18 22:23:46 +0200

    first update

--- src/layouts/LayoutGuest.vue
+++ src/layouts/LayoutGuest.vue
@@ -5,6 +5,7 @@ const styleStore = useStyleStore();
 </script>
 
 <template>
+  <!-- sd -->
   <div :class="{ dark: styleStore.darkMode }">
     <div class="bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
       <slot />
